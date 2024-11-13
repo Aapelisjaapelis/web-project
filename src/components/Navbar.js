b33cc1d18101281a741818502701a719a5541dae
AapoKiiskila <aapo.kiiskila@gmail.com>
2024-11-13 17:59:03 +0200

    Created a navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"
+
+function Navbar() {
+    const[menu, setMenu] = useState(false)
+
+    const showDropdownMenu = () => {
+        setMenu(!menu)
+    }
+
+    return (
+        <nav>
+
+            <button onClick={showDropdownMenu}>Menu</button>
+
+            <ul className={menu ? "show" : ""}>
+                <li>
+                    <Link to="">Public movie list</Link>
+                </li>
+                <li>
+                    <Link to="">Finnkino showtimes</Link>
+                </li>
+                <li>
+                    <Link to="">Groups</Link>
+                </li>
+                <li>
+                    <Link to="">Favorites</Link>
+                </li>
+                <li>
+                    <Link to="">Profile</Link>
+                </li>
+            </ul>
+            
+            <ul className={menu ? "show" : ""}>
+                <li>
+                    <Link to="">Log in</Link>
+                </li>
+                <li>
+                    <Link to="">Sign out</Link>
+                </li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import PublicMoviesList from './screens/PublicMoviesList';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PublicMoviesList />
+    <BrowserRouter>
+      <PublicMoviesList />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
