a83039e6eb2cf87c7c65b45a7f2433ccd936290c
Aapo Kiiskilä <aapo.kiiskila@gmail.com>
2024-11-14 15:38:13 +0200

    Register.js and Register.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import PublicMoviesList from './screens/PublicMoviesList';
+import Register from './screens/Register';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <PublicMoviesList />
+      <Register/>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,1 +1,26 @@
-import ".Register.css"
+import "./Register.css"
+import Navbar from "../components/Navbar.js"
+import React, { useState } from "react"
+
+function Register() {
+
+  return (
+    <>
+        <Navbar/>
+
+        <div>
+            <h1>Sign up</h1>
+            <form>
+                <label>Username:</label>
+                <input type="text"/>
+                <label>Email:</label>
+                <input type="email"/>
+                <label>Password:</label>
+                <input type="password"/>
+            </form>
+        </div>
+    </>
+  )
+}
+
+export default Register
