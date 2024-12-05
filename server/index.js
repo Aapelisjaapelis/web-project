aeeed1d79ef8097e186c16491b2b624d1facb29f
Martel <t2hema02@students.oamk.fi>
2024-12-05 22:55:22 +0200

    Some profile page stuff

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import userRouter from "./routers/userRouter.js"
 import groupRouter from "./routers/groupRouter.js"
+import profileRouter from "./routers/profileRouter.js"
 
 const port = process.env.PORT
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.urlencoded({extended: false}))
 
 app.use("/user", userRouter)
 app.use("/group", groupRouter)
+app.use("/profile", profileRouter)
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,9 +1,20 @@
 import './Profile.css';
-import Navbar from "../components/Navbar.js"
+import Navbar from "../components/Navbar.js";
+import { useUser } from "../context/useUser.js"
 
 function Profile() {
+
+  const { user } = useUser();
+
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <h1>Profile</h1>
+      <h2>{user.username}</h2>
+      <button>Change password</button>
+      <button>Change email address</button>
+      <button>Delete account</button>
+    </>
   );
 }
 
