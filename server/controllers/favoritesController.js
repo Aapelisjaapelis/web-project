b86b959cec4bf7a87a2b764e2d6c8e28e006198e
AapoKiiskila <aapo.kiiskila@gmail.com>
2024-12-06 12:26:30 +0200

    Using "movie_id" to search for movies from TMDB. Movies are then stored into "movieInfo".

--- server/controllers/favoritesController.js
+++ server/controllers/favoritesController.js
@@ -1,11 +1,11 @@
 import { selectMyFavoriteMovies } from '../models/Favorites.js'
+import { emptyOrRows } from '../helpers/utils.js'
 
 const getMyFavoriteMovies = async(req, res, next) => {
     try {
         const result = await selectMyFavoriteMovies(req.params.id)
-        console.log(result.rows)
-
-    } catch(error) {
+        return res.status(200).json(emptyOrRows(result))
+    } catch (error) {
         return next(error)
     }
 }
--- src/screens/FavoriteMoviesList.js
+++ src/screens/FavoriteMoviesList.js
@@ -8,21 +8,56 @@ const url = process.env.REACT_APP_API_URL
 
 function FavoriteMoviesList() {
   const {user} = useUser();
-  const [favMovies, setFavMovies] = useState([])
+  const [favMoviesId, setFavMoviesId] = useState([])
+  const [movieInfo, setMovieInfo] = useState([])
 
   useEffect(() => {
     getFavMoviesId()
   }, [])
 
-  const getFavMoviesId = async () => {
-    try {
-      const response = await axios.get(url + "/favorites/myFavorites/" + user.id)
-      
-    } catch(error) {
-      alert(error)
-    }
+  const getFavMoviesId = () => {
+    axios
+      .get(url + "/favorites/myFavorites/" + user.id)
+      .then(response => {
+        setFavMoviesId(response.data)
+      })
+      .catch(error => {
+        alert(error)
+    })
   }
-  
+
+  useEffect(() => {
+    if (favMoviesId.length !== 0) {
+      getFavMoviesInfo();
+    }
+  }, [favMoviesId])
+
+  const getFavMoviesInfo = () => {
+    const idArray = favMoviesId.map((favMovieId) => favMovieId.movie_id)
+    const movieInfoList = []
+
+    idArray.map(id => { 
+      const options = {
+      method: "GET",
+      url: "https://api.themoviedb.org/3/movie/" + id,
+      headers: {
+        accept: "application/json",
+        Authorization: process.env.BEARER
+      }
+    }
+    
+    axios
+      .request(options)
+      .then(response => {
+        movieInfoList.push(response.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    })
+
+    setMovieInfo(movieInfoList)
+  }
 
   return (
     <>
