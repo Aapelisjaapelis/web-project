a72b10f3bf9ce78673851b6bdf292db423cde8a4
AapoKiiskila <aapo.kiiskila@gmail.com>
2024-12-09 20:38:44 +0200

    Insert favorite movies for a user

---
+++ server/insertFavorites.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.favorites(
+	account_id, movie_id, movie_name)
+	VALUES (1, 65436, 'Class Dismissed: How TV Frames the Working Class');
+
+INSERT INTO public.favorites(
+	account_id, movie_id, movie_name)
+	VALUES (1, 65, '8 Mile');
+
+INSERT INTO public.favorites(
+	account_id, movie_id, movie_name)
+	VALUES (1, 6511, 'The Consequence');
+
+INSERT INTO public.favorites(
+	account_id, movie_id, movie_name)
+	VALUES (1, 65118, 'Interceptors');
