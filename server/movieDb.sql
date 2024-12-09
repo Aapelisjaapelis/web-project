577dde390488d4696561fbbbd2db3e8cfcedf1b5
AapoKiiskila <aapo.kiiskila@gmail.com>
2024-12-09 20:35:52 +0200

    Modified "favorites" table by adding a "poster_path" field

--- server/movieDb.sql
+++ server/movieDb.sql
@@ -26,6 +26,7 @@ create table favorites (
     account_id serial not null,
     movie_id int not null,
     movie_name varchar(100) not null,
+    poster_path varchar(100),
     foreign key (account_id) references account(account_id)
 );
 
