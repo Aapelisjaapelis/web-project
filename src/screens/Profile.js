ff99fc0c4466aa71b8fabfe3bb82fdc854fef068
Aapo Kiiskilä <aapo.kiiskila@gmail.com>
2024-12-11 16:18:41 +0200

    Layout for profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -14,28 +14,39 @@ function Profile() {
   return (
     <>
       <Navbar />
-      <div className='profile'>
-        <h1>Profile</h1>
-        <h2>Username: {user.username}</h2>
-        <button className="changePassword" onClick={e =>  navigate('/changePassword')}>Change password</button>
-        <button className="changeEmail" onClick={e =>  navigate('/changeEmail')}>Change email address</button>
-        <Popup trigger=
-              {<button>Delete account</button>}
-          modal nested>
-          {
-              close => (
-                  <div>
-                      <div>
-                        <label for="newDesc">Are you sure you want to delete your account and all the information related</label><br/>
-                      </div>
-                      <div>
-                          <button onClick={e => navigate('/profile')}>Submit</button>
-                      </div>
-                  </div>
-              )
-          }
-        </Popup>
+      <h1 id="profileHeader">Profile</h1>
+      <div id="profile">
+
+        <div id="leftProfile">
+          <button id="changePassword" onClick={e =>  navigate('/changePassword')}>Change password</button>
+          <button id="changeEmail" onClick={e =>  navigate('/changeEmail')}>Change email address</button>
+          <Popup trigger=
+                {<button id="deleteAccount">Delete account</button>}
+            modal nested>
+            {
+                close => (
+                    <div>
+                        <div>
+                          <label for="newDesc">Are you sure you want to delete your account and all the information related</label><br/>
+                        </div>
+                        <div>
+                            <button onClick={e => navigate('/profile')}>Submit</button>
+                        </div>
+                    </div>
+                )
+            }
+          </Popup>
+        </div>
+
+        <div id="middleProfile">
+          <h2>Username: {user.username}</h2> 
+        </div>
+
+        <div id="rightProfile"></div>
+
       </div>
+
+      <div></div>
     </>
   );
 }
